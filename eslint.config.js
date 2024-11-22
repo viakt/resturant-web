d35536f1b1e314254761a993abb29231e23b4696
viakt <viakt@qq.com>
2024-11-23 02:41:33 +0800

    添加eslint规则

--- eslint.config.js
+++ eslint.config.js
@@ -4,6 +4,7 @@ import pluginVue from 'eslint-plugin-vue'
 import pluginPrettierRecommendedConfigs from 'eslint-plugin-prettier/recommended'
 import globals from 'globals'
 import parserVue from 'vue-eslint-parser'
+import prettier from 'eslint-plugin-prettier'
 
 export default [
 	pluginJs.configs.recommended,
@@ -22,6 +23,36 @@ export default [
 			parserOptions: {
 				parser: tseslint.parser,
 			},
+		},
+	},
+	{
+		plugins: {
+			prettier,
+		},
+		rules: {
+			// 开启这条规则后，会将prettier的校验规则传递给eslint，这样eslint就可以按照prettier的方式来进行代码格式的校验
+			'prettier/prettier': 'error',
+			// eslint（https://eslint.bootcss.com/docs/rules/）
+			'no-var': 'error', // 要求使用 let 或 const 而不是 var
+			'no-multiple-empty-lines': ['warn', { max: 1 }], // 不允许多个空行
+			'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+			'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+			'no-unexpected-multiline': 'error', // 禁止空余的多行
+			'no-useless-escape': 'off', // 禁止不必要的转义字符
+
+			// typeScript (https://typescript-eslint.io/rules)
+			'@typescript-eslint/no-unused-vars': 'error', // 禁止定义未使用的变量
+			'@typescript-eslint/prefer-ts-expect-error': 'error', // 禁止使用 @ts-ignore
+			'@typescript-eslint/no-explicit-any': 'off', // 禁止使用 any 类型
+			'@typescript-eslint/no-non-null-assertion': 'off',
+			'@typescript-eslint/no-namespace': 'off', // 禁止使用自定义 TypeScript 模块和命名空间。
+			'@typescript-eslint/semi': 'off',
+
+			// eslint-plugin-vue (https://eslint.vuejs.org/rules/)
+			'vue/multi-word-component-names': 'off', // 要求组件名称始终为 “-” 链接的单词
+			'vue/script-setup-uses-vars': 'error', // 防止<script setup>使用的变量<template>被标记为未使用
+			'vue/no-mutating-props': 'off', // 不允许组件 prop的改变
+			'vue/attribute-hyphenation': 'off', // 对模板中的自定义组件强制执行属性命名样式
 		},
 	},
 ]
